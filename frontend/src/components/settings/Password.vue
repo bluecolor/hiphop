b47511f981e2742350781b26a9ff4db8d8590545
ceyhun kerti <ceyhun.kerti@gmail.com>
2018-03-02 16:44:30 +0300

    settings

---
+++ frontend/src/components/settings/Password.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+    v-flex(mt-5)
+      v-card.elevation-5
+        v-toolbar(color="white")
+          v-btn(icon="" @click="onBack")
+            v-icon arrow_back
+          v-toolbar-title Change password
+        v-card-text
+          v-form
+            v-text-field(
+              label='Current password',
+              v-model='p.current',
+              :rules="[rules.required]"
+              hint="At least 8 characters"
+              min="8"
+              :append-icon="e1 ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (e1 = !e1)"
+              :type="e1 ? 'password' : 'text'"
+              counter
+            )
+            v-text-field(
+              label='New password',
+              v-model='p.new',
+              :rules="[rules.required]"
+              hint="At least 8 characters"
+              min="8"
+              :append-icon="e1 ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (e1 = !e1)"
+              :type="e1 ? 'password' : 'text'"
+              counter
+            )
+            v-text-field(
+              label='Re new password',
+              v-model='p.reNew',
+              :rules="[rules.required]"
+              hint="At least 8 characters"
+              min="8"
+              :append-icon="e1 ? 'visibility' : 'visibility_off'"
+              :append-icon-cb="() => (e1 = !e1)"
+              :type="e1 ? 'password' : 'text'"
+              counter
+            )
+        v-card-actions
+          v-spacer
+          v-btn(flat) Save
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      e1: false,
+      rules: {
+        required: (value) => !!value || ''
+      },
+      p: {
+        current: undefined,
+        new: undefined,
+        reNew: undefined
+      }
+    }
+  },
+  methods: {
+    onBack () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
--- frontend/src/components/settings/Settings.vue
+++ frontend/src/components/settings/Settings.vue
@@ -5,52 +5,86 @@
         v-btn(icon="" @click.native="display = false" dark="")
           v-icon close
         v-toolbar-title Settings
-        v-spacer
-        v-toolbar-items
-          v-btn(dark="" flat="" @click.native="dialog = false") Save
-      v-list(three-line="" subheader="")
-        v-subheader User Controls
-        v-list-tile(avatar="")
-          v-list-tile-content
-            v-list-tile-title Content filtering
-            v-list-tile-sub-title Set the content filtering level to restrict apps that can be downloaded
-        v-list-tile(avatar="")
-          v-list-tile-content
-            v-list-tile-title Password
-            v-list-tile-sub-title Require password for purchase or use password to restrict purchase
-      v-divider
-      v-list(three-line="" subheader="")
-        v-subheader General
-        v-list-tile(avatar="")
-          v-list-tile-action
-            v-checkbox(v-model="notifications")
-          v-list-tile-content
-            v-list-tile-title Notifications
-            v-list-tile-sub-title Notify me about updates to apps or games that I downloaded
-        v-list-tile(avatar="")
-          v-list-tile-action
-            v-checkbox(v-model="sound")
-          v-list-tile-content
-            v-list-tile-title Sound
-            v-list-tile-sub-title Auto-update apps at any time. Data charges may apply
-        v-list-tile(avatar="")
-          v-list-tile-action
-            v-checkbox(v-model="widgets")
-          v-list-tile-content
-            v-list-tile-title Auto-add widgets
-            v-list-tile-sub-title Automatically add home screen widgets
-
+      div(
+        v-show="screen==='password'"
+        style="max-width: 700px; margin: auto;"
+        class="lighten-3"
+      )
+        password(v-on:close="screen='settings'")
+      div(
+        v-show="screen==='settings'"
+        style="max-width: 700px; margin: auto;"
+        class="lighten-3"
+      )
+          v-list(one-line subheader)
+            v-subheader General
+            v-card.elevation-5
+              v-list-tile(avatar ripple @click="screen = 'password'" )
+                v-list-tile-content
+                  v-list-tile-title Password
+                  v-list-tile-sub-title Change your account's password
+          v-list(one-line="" subheader="")
+            v-subheader User Controls
+            v-card.elevation-5
+              v-list-tile(avatar)
+                v-list-tile-content
+                  v-list-tile-title Footer
+                  v-list-tile-sub-title Display footer
+                v-list-tile-action
+                  v-switch(v-model="notifications" color="success")
+              v-divider
+              v-list-tile(avatar)
+                v-list-tile-content
+                  v-list-tile-title Mini variant
+                  v-list-tile-sub-title Display left drawer as mini variant
+                v-list-tile-action
+                  v-switch(v-model="miniVariant" color="success")
+              v-divider
+              v-list-tile(avatar)
+                v-list-tile-content
+                  v-list-tile-title Desktop notifications
+                  v-list-tile-sub-title Display desktop notifications for the events
+                v-list-tile-action
+                  v-switch(v-model="desktopNotifications" color="success")
+              v-divider
+              v-list-tile(avatar)
+                v-list-tile-content
+                  v-list-tile-title Sound
+                  v-list-tile-sub-title Play sound when the event occurs
+                v-list-tile-action
+                  v-switch(v-model="sound" color="success")
+          v-list(one-line subheader)
+            v-subheader Administration
+            v-card.elevation-5
+              v-list-tile(avatar ripple @click="")
+                v-list-tile-content
+                  v-list-tile-title Mail
+                  v-list-tile-sub-title Mail settings for notifications
+              v-divider
+              v-list-tile(avatar ripple @click="")
+                v-list-tile-content
+                  v-list-tile-title Slack
+                  v-list-tile-sub-title Configure slack hook for instant messages
+              v-divider
+              v-list-tile(avatar ripple @click="")
+                v-list-tile-content
+                  v-list-tile-title Updates
+                  v-list-tile-sub-title Check for updates
 </template>
 
 <script>
+  import Password from './Password'
+
   export default {
     props: ['show'],
     data () {
       return {
         display: JSON.parse(this.show),
         notifications: false,
+        desktopNotifications: false,
         sound: true,
-        widgets: false
+        miniVariant: true,
+        screen: 'settings'
       }
     },
     computed: {
@@ -66,6 +100,9 @@
       }
     },
     mounted () {
+    },
+    components: {
+      Password
     }
   }
 </script>
