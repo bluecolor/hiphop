47e3d59b7c15ec7d0fd690a0ba62d0f74fca78f0
ceyhun kerti <ceyhun.kerti@gmail.com>
2018-03-14 01:13:31 +0300

    on export via toolbar

--- frontend/src/components/query/Query.vue
+++ frontend/src/components/query/Query.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 
-//- div(style="width:100%;")
 div.query(xs12 mt-1 style="height:100%")
   v-navigation-drawer(mt-3 fixed v-model="queryRightDrawer" right="" clipped app style="z-index:0;")
     v-container.drawer-container
@@ -207,111 +206,6 @@ import 'codemirror/addon/hint/sql-hint.js'
 export default {
   data () {
     return {
-      // headers: [
-      //   {
-      //     text: 'Dessert (100g serving)',
-      //     align: 'left',
-      //     sortable: false,
-      //     value: 'name'
-      //   },
-      //   { text: 'Calories', value: 'calories' },
-      //   { text: 'Fat (g)', value: 'fat' },
-      //   { text: 'Carbs (g)', value: 'carbs' },
-      //   { text: 'Protein (g)', value: 'protein' },
-      //   { text: 'Iron (%)', value: 'iron' }
-      // ],
-      items: [
-        {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%'
-        },
-        {
-          value: false,
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%'
-        },
-        {
-          value: false,
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%'
-        },
-        {
-          value: false,
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%'
-        },
-        {
-          value: false,
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%'
-        },
-        {
-          value: false,
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%'
-        },
-        {
-          value: false,
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%'
-        },
-        {
-          value: false,
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%'
-        },
-        {
-          value: false,
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%'
-        }
-      ],
       historyItemMenu: [{
         title: 'Copy query to clipboard',
         id: 'COPY_CLIP'
@@ -576,8 +470,8 @@ export default {
         this.console.content = 'grid'
       })
     },
-    onExport () {
-      const query = this.editor.getValue()
+    onExport (q) {
+      const query = q || this.editor.getValue()
       if (_.isEmpty(query)) {
         this.snackWarning('Nothing to export')
         return
@@ -662,6 +556,11 @@ export default {
       this.snackInfo('Copied log message')
     },
     download () {
+      if (this.result.query.query) {
+        this.onExport(this.result.query.query)
+      } else {
+        this.snackWarning('Unable to get query')
+      }
     }
   },
   components: {
