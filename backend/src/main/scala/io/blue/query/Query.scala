e9b5850cf839257a394a1f1eb733023e12611b8a
ceyhun kerti <ceyhun.kerti@gmail.com>
2018-07-01 22:27:58 +0300

    latest

---
+++ backend/src/main/scala/io/blue/query/Query.scala
@@ -0,0 +1,70 @@
+package io.blue.query
+
+import java.util.regex.{Pattern,Matcher}
+import org.springframework.stereotype.Component
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.lang.invoke.MethodHandles
+import scala.collection.mutable.ListBuffer
+
+trait Query {
+
+  private val log:Logger  = LoggerFactory.getLogger(MethodHandles.lookup.lookupClass)
+  protected val searchPattern = "(\\w+\\s+)|(\\s*\\w+\\s*)(:#|:|~|<|>|!:)(\\s*\\w+\\s*|\\[[\\w+[\\s*\\w*]*,??]*\\]\\s*)"
+  protected val pattern = Pattern.compile(searchPattern)
+  
+  protected def parseSearchPattern(str: String):( String, Map[String, AnyRef] )  = {
+    val search = if(str == null) "" else str.trim
+    if(search.isEmpty) return (" (1=1) ",null)
+    log.debug(search)
+    val (f,o) = buildFilter(pattern.matcher(search+" "))
+    if(f.isEmpty) 
+      ("(1=1)", null) 
+    else
+      (f.map(f => s"(${f})").mkString(" and "), o)
+  }
+
+  protected def build(p:String, o: String, v:String): String = {
+    var op = new Operation
+    var parameter= p
+    var value = v.stripPrefix("[").stripSuffix("]")
+
+    if(o == SearchOperation.LIKE){
+      return value
+      .split(",")
+      .map(vl => s"""${parameter.toLowerCase} like '%${vl.trim.toLowerCase.replace("*", "%")}%'""")
+      .mkString(" or ")
+    }
+    if(o == SearchOperation.EQUAL_ID){
+      return s"${parameter} in (${value})"
+    }
+    if(o == SearchOperation.EQUAL){
+      return s"${parameter.toLowerCase} in (" + 
+        value.split(",").map(vl=> s"'${vl}'").mkString(",") + 
+      ")"
+    }
+    else return "1=1"
+  }
+
+  protected def buildFilter(m: Matcher): ( List[String], Map[String, AnyRef] )
+
+
+
+  protected def orderBy(sortBy: String, order: String) = {
+    val s = if(sortBy==null) "name" else sortBy.toLowerCase
+    val o = if(order ==null) "asc" else order.toLowerCase
+    val x = if(Array("asc","desc") contains o) o else "asc"
+    val q = sortBy match {
+      case "name" => "t.name"
+      case "avgd" => "s.avgDuration"
+      case "error"=> "s.error"
+      case _ => "t.name"
+    }  
+    
+    s"order by $q $x"
+  }
+
+}
---
+++ backend/src/main/scala/io/blue/query/SearchOperation.scala
@@ -0,0 +1,36 @@
+package io.blue.query
+
+import scala.collection.JavaConversions._
+
+object SearchOperation {
+
+  val LIKE         = "~"
+  val EQUAL        = ":"
+  val EQUAL_ID     = ":#"
+  val NOT_EQUAL    = "!:"
+  val NEGATION     = "!"
+  val GREATER_THAN = ">"
+  val LESS_THAN    = "<" 
+  val IN           = "~:"    
+  val CONTAINS     = "::"
+
+  val SQL_EQUAL:String    = "="
+  val SQL_LIKE:String     = "like"
+  val SQL_IN:String       = "IN"
+  val SQL_NOT_IN:String   = "NOT IN"
+  val SQL_NOT_EQUAL:String= "NOT_EQUAL"
+  val SQL_GREATER_THAN    = ">"
+  val SQL_LESS_THAN       = "<" 
+
+  val operator = Map(
+    LIKE          -> "like",
+    EQUAL         -> "=",
+    NOT_EQUAL     -> "!=",
+    EQUAL_ID      -> "=",
+    GREATER_THAN  -> ">",
+    LESS_THAN     -> "<",
+    IN            -> "in",
+    CONTAINS      -> "in"
+  )
+
+}
